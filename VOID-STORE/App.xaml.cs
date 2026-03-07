cc9124cf877b7f9ec9172a2d40804a3b440974a5
Hakan Tuna <ht.tunahakan@gmail.com>
2026-03-08 00:20:07 +0300

    Add project files.

---
+++ VOID-STORE/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace VOID_STORE
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
